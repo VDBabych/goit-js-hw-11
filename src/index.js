55f3af288292296db498a2a4eef3f900d60675a8
Volodymyr Babych <v.d.babych@gmail.com>
2022-11-25 12:25:11 +0200

    add class

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import axios from 'axios';
+import { PixabayApi } from './js/get-images';
 import { createMarkup } from './js/create-markup';
 
-//API variables
-const API_KEY = "31574870-ec3306c679007fa4646c6ce9c"
-const BASE_URL = "https://pixabay.com/api/"
-let page = 1;
-let query = '';
-let avaliablePages = 0;
-const perPage = 40;
+const pixabayApi = new PixabayApi();
 
 //page elements
 const formEl = document.querySelector('.search-form');
@@ -20,12 +14,12 @@ formEl.addEventListener('submit', onFormSubmit);
 loadMoreBtnEl.addEventListener('click', onLoadMoreBtnClick);
 
 async function onLoadMoreBtnClick(e) {
-    page += 1;
-    if (page === avaliablePages) {
+    pixabayApi.pageIncrement();
+    if (pixabayApi.getPage() === pixabayApi.getAvaliablePages()) {
         loadMoreBtnEl.classList.add('is-hidden');
     }
     try {
-        const data = await getImages(query);
+        const data = await pixabayApi.getImages();
         const additionalGalleryMarkup = createMarkup(data.hits);
         galleryEl.insertAdjacentHTML('beforeend', additionalGalleryMarkup);
         
@@ -35,20 +29,20 @@ async function onLoadMoreBtnClick(e) {
 }
 
 async function onFormSubmit(e) {
-    page = 1;
     e.preventDefault();
-    query = e.target.elements.searchQuery.value.trim();
+    pixabayApi.setPageToDefault();
+    const pureInputText = e.target.elements.searchQuery.value.trim();
+    pixabayApi.setQuery(pureInputText)
     e.target.reset();
     try {
-        const data = await getImages(query);
+        const data = await pixabayApi.getImages();
         if (data.totalHits === 0) {
             galleryEl.innerHTML = '';
             Notify.failure("Sorry, there are no images matching your search query. Please try again.");
             return
         }
         const galleryMarkup = createMarkup(data.hits);
-        avaliablePages = Math.floor(data.totalHits / perPage);
-        if (avaliablePages > 1) {
+        if (pixabayApi.getAvaliablePages() > 1) {
             loadMoreBtnEl.classList.remove('is-hidden')
         }
         galleryEl.innerHTML = galleryMarkup;
@@ -57,10 +51,5 @@ async function onFormSubmit(e) {
         Notify.failure(error.message)
     }
 
-}
-
-async function getImages(q) {
-    const { data } = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${q}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`);
-    return data;
 }
 
